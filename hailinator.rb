76acbe7005fbaea76bd24a1ceef69d2cb3866ecb
MakerSquare Student <student@challenge-1.att.net>
2013-06-20 11:08:35 -0500

    Finished Hailinator

--- hailinator.rb
+++ hailinator.rb
@@ -7,23 +7,15 @@ Twitter.configure do |config|
   config.oauth_token_secret = "3t0tEKClCyARFd06NxaCt7YQPHHCWV1zH18Hk6aVE"
 end
 
-Twitter.home_timeline
+require 'csv'
 
-# results_array = []
+CSV.open("users_with_hail_damage.csv", "w") do |csv|
+  		csv << ["handle", "text", "url"]
 
-Twitter.search("#hail #damage").results.map do |status|
-	puts "#{status.from_user}: #{status.text}"
-
-end
-
-# result_array.each do |result|
-# 	puts result 
-# end
-
-
-# require 'csv'
-# CSV.open("users_with_hail_damage.csv", "w") do |csv|
-#   csv << ["row", "of", "CSV", "data"]
-#   csv << ["another", "row"]
-#   # ...
-# end
+  		Twitter.search("#hail #damage").results.map do |status|
+			puts "#{status.from_user}: #{status.text}"
+			puts status.inspect
+		
+		csv <<  [status.user.screen_name, status.text, "https://twitter.com/#{status.user.screen_name}/statuses/#{status.id}"]
+		end
+ end
